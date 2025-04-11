e51c2704e8f7a1fc0614ba47b18d9f5cf72d3fff
mohamadMurad <m.murad@staff.ososs.com>
2025-04-11 21:23:08 +0300

    fix component name

--- resources/js/pages/admin/roles/index.tsx
+++ resources/js/pages/admin/roles/index.tsx
@@ -10,61 +10,62 @@ import { Button } from '@/components/ui/button';
 import { EyeIcon, PenIcon } from 'lucide-react';
 import DeletePopover from '@/components/murad/DeletePopover';
 
-
-export default function index() {
+export default function Index() {
     const { items } = usePage<SharedData<{ items: Pagination }>>().props;
     const resource: string = 'roles';
 
     const breadcrumbs: BreadcrumbItem[] = [
         {
             title: t('attributes.dashboard'),
-            href: route('dashboard')
+            href: route('dashboard'),
         },
         {
             title: t(`attributes.${resource}.title`),
-            href: route('roles.index')
-        }
+            href: route('roles.index'),
+        },
     ];
-
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={t(`attributes.${resource}.title`)} />
             <div className="px-4 py-6">
-                <MDatatable items={items} resource={resource} columns={[
-                    {
-                        accessorKey: 'name',
-                        header: t('attributes.name')
-                    },
-                    {
-                        accessorKey: 'created_at',
-                        header: t('attributes.created_at')
-                    },
-                    {
-                        id: 'actions',
-                        header: t('attributes.actions'),
-                        cell: ({ row }: { row: Row<any> }) => {
-                            const rowModel = row.original as unknown as RoleInterface;
+                <MDatatable
+                    items={items}
+                    resource={resource}
+                    columns={[
+                        {
+                            accessorKey: 'name',
+                            header: t('attributes.name'),
+                        },
+                        {
+                            accessorKey: 'created_at',
+                            header: t('attributes.created_at'),
+                        },
+                        {
+                            id: 'actions',
+                            header: t('attributes.actions'),
+                            cell: ({ row }: { row: Row<any> }) => {
+                                const rowModel = row.original as unknown as RoleInterface;
 
-                            return (
-                                <div className="flex ">
-                                    <Link className="m-0" href={route(resource + '.show', rowModel.id)}>
-                                        <Button variant="ghost">
-                                            <EyeIcon size={'20'} />
-                                        </Button>
-                                    </Link>
-                                    <Link className="m-0" href={route(resource + '.edit', rowModel.id)}>
-                                        <Button variant="ghost">
-                                            <PenIcon size={'20'} />
-                                        </Button>
-                                    </Link>
-                                    <DeletePopover id={rowModel.id} resource={resource} />
-
-                                </div>
-                            );
-                        }
-                    }
-                ]} />
+                                return (
+                                    <div className="flex">
+                                        <Link className="m-0" href={route(resource + '.show', rowModel.id)}>
+                                            <Button variant="ghost">
+                                                <EyeIcon size={'20'} />
+                                            </Button>
+                                        </Link>
+                                        <Link className="m-0" href={route(resource + '.edit', rowModel.id)}>
+                                            <Button variant="ghost">
+                                                <PenIcon size={'20'} />
+                                            </Button>
+                                        </Link>
+                                        <DeletePopover id={rowModel.id} resource={resource} />
+                                    </div>
+                                );
+                            },
+                        },
+                    ]}
+                />
             </div>
         </AppLayout>
     );
--- resources/js/pages/admin/users/index.tsx
+++ resources/js/pages/admin/users/index.tsx
@@ -11,62 +11,62 @@ import { Button } from '@/components/ui/button';
 import DeletePopover from '@/components/murad/DeletePopover';
 import { Row } from '@tanstack/react-table';
 
-
-
-export default function index() {
+export default function Index() {
     const { items } = usePage<SharedData<{ items: Pagination }>>().props;
     const resource: string = 'users';
 
     const breadcrumbs: BreadcrumbItem[] = [
         {
             title: t('attributes.dashboard'),
-            href: route('dashboard')
+            href: route('dashboard'),
         },
         {
             title: t(`attributes.${resource}.title`),
-            href: route('roles.index')
-        }
+            href: route('roles.index'),
+        },
     ];
-
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={t(`attributes.${resource}.title`)} />
             <div className="px-4 py-6">
-                <MDatatable items={items} resource={resource} columns={[
-                    {
-                        accessorKey: 'name',
-                        header: t('attributes.name')
-                    },
-                    {
-                        accessorKey: 'email',
-                        header: t('attributes.email')
-                    },
-                    {
-                        id: 'actions',
-                        header: t('attributes.actions'),
-                        cell: ({ row }: { row: Row<any> }) => {
-                            const rowModel = row.original as unknown as RoleInterface;
+                <MDatatable
+                    items={items}
+                    resource={resource}
+                    columns={[
+                        {
+                            accessorKey: 'name',
+                            header: t('attributes.name'),
+                        },
+                        {
+                            accessorKey: 'email',
+                            header: t('attributes.email'),
+                        },
+                        {
+                            id: 'actions',
+                            header: t('attributes.actions'),
+                            cell: ({ row }: { row: Row<any> }) => {
+                                const rowModel = row.original as unknown as RoleInterface;
 
-                            return (
-                                <div className="flex ">
-                                    <Link className="m-0" href={route(resource + '.show', rowModel.id)}>
-                                        <Button variant="ghost">
-                                            <EyeIcon size={'20'} />
-                                        </Button>
-                                    </Link>
-                                    <Link className="m-0" href={route(resource + '.edit', rowModel.id)}>
-                                        <Button variant="ghost">
-                                            <PenIcon size={'20'} />
-                                        </Button>
-                                    </Link>
-                                    <DeletePopover id={rowModel.id} resource={resource} />
-
-                                </div>
-                            );
-                        }
-                    }
-                ]} />
+                                return (
+                                    <div className="flex">
+                                        <Link className="m-0" href={route(resource + '.show', rowModel.id)}>
+                                            <Button variant="ghost">
+                                                <EyeIcon size={'20'} />
+                                            </Button>
+                                        </Link>
+                                        <Link className="m-0" href={route(resource + '.edit', rowModel.id)}>
+                                            <Button variant="ghost">
+                                                <PenIcon size={'20'} />
+                                            </Button>
+                                        </Link>
+                                        <DeletePopover id={rowModel.id} resource={resource} />
+                                    </div>
+                                );
+                            },
+                        },
+                    ]}
+                />
             </div>
         </AppLayout>
     );
