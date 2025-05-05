2eb7c1b1773dd4ffcf0076b0b17458cb70c502c2
mohamadMurad <m.murad@staff.ososs.com>
2025-05-06 00:59:03 +0300

    update sidebar to support group

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -40,47 +40,12 @@ function NavItems({ items = [] }: { items: NavItem[] }) {
     return (
         <>
             {items.map((item) => {
-                if (item.type === 'collapsible' && item.items) {
-                    const IconComponent = iconMapping[item.icon as string] || LayoutGrid;
-
-                    return (
-                        <Collapsible key={item.title} className="group/collapsible">
-                            <SidebarMenuItem>
-                                <CollapsibleTrigger asChild>
-                                    <SidebarMenuButton tooltip={{ children: item.title }}>
-                                        {item.icon && <IconComponent />}
-                                        <span>{item.title}</span>
-                                        <ChevronUp className="ml-auto transition-transform group-data-[state=closed]/collapsible:rotate-90 group-data-[state=open]/collapsible:rotate-180" />
-                                    </SidebarMenuButton>
-                                </CollapsibleTrigger>
-
-                                <CollapsibleContent>
-                                    <SidebarMenuSub>
-                                        {item.items.map((subItem) =>
-                                            subItem.type === 'item' ? (
-                                                <SidebarMenuSubItem key={subItem.title}>
-                                                    <SidebarMenuButton asChild isActive={subItem.isActive} tooltip={{ children: subItem.title }}>
-                                                        <Link href={subItem.href} prefetch>
-                                                            {subItem.icon && (
-                                                                <span className="mr-2">
-                                                                    {React.createElement(iconMapping[subItem.icon] || LayoutGrid)}
-                                                                </span>
-                                                            )}
-                                                            <span>{subItem.title}</span>
-                                                        </Link>
-                                                    </SidebarMenuButton>
-                                                </SidebarMenuSubItem>
-                                            ) : null,
-                                        )}
-                                    </SidebarMenuSub>
-                                </CollapsibleContent>
-                            </SidebarMenuItem>
-                        </Collapsible>
-                    );
-                }
-
                 if (item.type === 'item') {
                     return <NavItem key={item.title} item={item} />;
+                }
+
+                if (item.type === 'collapsible' && item.items) {
+                    return <NavCollapsibleItem key={item.title} item={item} />;
                 }
 
                 return null;
@@ -100,5 +65,42 @@ function NavItem({ item }: { item: NavItem }) {
                 </Link>
             </SidebarMenuButton>
         </SidebarMenuItem>
+    );
+}
+
+function NavCollapsibleItem({ item }: { item: NavItem }) {
+    const IconComponent = iconMapping[item.icon as string] || LayoutGrid;
+    const isAnyChildActive = item.items?.some((subItem) => subItem.isActive);
+    return (
+        <Collapsible key={item.title} className="group/collapsible" open={isAnyChildActive}>
+            <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={{ children: item.title }}>
+                        {item.icon && <IconComponent />}
+                        <span>{item.title}</span>
+                        <ChevronUp className="ml-auto transition-transform group-data-[state=closed]/collapsible:rotate-90 group-data-[state=open]/collapsible:rotate-180" />
+                    </SidebarMenuButton>
+                </CollapsibleTrigger>
+
+                <CollapsibleContent>
+                    <SidebarMenuSub>
+                        {item.items?.map((subItem) =>
+                            subItem.type === 'item' ? (
+                                <SidebarMenuSubItem key={subItem.title}>
+                                    <SidebarMenuButton asChild isActive={subItem.isActive} tooltip={{ children: subItem.title }}>
+                                        <Link href={subItem.href} prefetch>
+                                            {subItem.icon && (
+                                                <span className="mr-2">{React.createElement(iconMapping[subItem.icon] || LayoutGrid)}</span>
+                                            )}
+                                            <span>{subItem.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuSubItem>
+                            ) : null,
+                        )}
+                    </SidebarMenuSub>
+                </CollapsibleContent>
+            </SidebarMenuItem>{' '}
+        </Collapsible>
     );
 }
