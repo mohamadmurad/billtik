5377e827254c669446f47f1a14c94b532845821e
mohamadMurad <m.murad@staff.ososs.com>
2025-07-10 01:04:58 +0300

    add phone number input

---
+++ resources/js/components/murad/PhoneInput.tsx
@@ -0,0 +1,27 @@
+import '$/phoneInputStyle.css';
+import InputError from '@/components/input-error';
+import { Label } from '@/components/ui/label';
+import * as React from 'react';
+import PhoneInputLib, { PhoneInputProps } from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+
+type InputProps = PhoneInputProps &
+    React.ComponentProps<'input'> & {
+        label?: string;
+        error?: string;
+        hideLabel?: boolean;
+        hideError?: boolean;
+    };
+export default function PhoneInput({ className, label, error, country, onlyCountries, hideLabel = false, hideError = false, ...props }: InputProps) {
+    return (
+        <>
+            {!hideLabel && label && (
+                <Label htmlFor={props.id || props.name} className="flex items-center gap-1">
+                    {label} {props.required && <span className="text-destructive text-sm leading-none dark:text-red-400">*</span>}
+                </Label>
+            )}
+            <PhoneInputLib country={country} onlyCountries={onlyCountries} showDropdown={true} {...props} />
+            {!hideError && error && <InputError className="mt-2" message={error} />}
+        </>
+    );
+}
