664e4c049b84734ae500d0c5fee59e45c39b4925
mohamadMurad <m.murad@staff.ososs.com>
2025-07-09 20:11:48 +0300

    update m-datatable.tsx

--- resources/js/components/murad/m-datatable.tsx
+++ resources/js/components/murad/m-datatable.tsx
@@ -1,25 +1,44 @@
-import { Link } from '@inertiajs/react';
-import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/data-table';
+import { Button } from '@/components/ui/button';
+import { Link } from '@inertiajs/react';
 
-import { Pagination } from '@/types/pagination';
 import { t } from '@/hooks/useTranslation';
+import { Pagination } from '@/types/pagination';
 import { ColumnDef } from '@tanstack/react-table';
+import { ReactNode } from 'react';
 
-export default function MDatatable({ items, resource ,columns}: { items: Pagination, resource: string ,columns: ColumnDef<any,any>[]}) {
-
+export default function MDatatable({
+    items,
+    resource,
+    columns,
+    withHeader = true,
+    headerContent = null,
+    createButton = true,
+}: {
+    items: Pagination;
+    resource: string;
+    withHeader: boolean;
+    headerContent: null | ReactNode;
+    columns: ColumnDef<never, never>[];
+    createButton?: boolean;
+}) {
     return (
         <>
-            <div className="flex justify-between mb-5">
-                <div></div>
-                <div className="">
-                    <Link href={route(resource+'.create')}>
-                        <Button variant="default" className="cursor-pointer">
-                            {t('attributes.create')} {t('attributes.'+resource+'.title')}
-                        </Button>
-                    </Link>
+            {withHeader && (
+                <div className="mb-5 flex items-center justify-between">
+                    <div>{headerContent}</div>
+                    <div className="">
+                        {createButton && (
+                            <Link href={route(resource + '.create')}>
+                                <Button variant="default" className="faateng-btn-primary px-8 py-5 text-[16px] font-extrabold">
+                                    {t('attributes.create')} {t('attributes.' + resource + '.create_title')}
+                                </Button>
+                            </Link>
+                        )}
+                    </div>
                 </div>
-            </div>
+            )}
+
             <DataTable
                 columns={columns}
                 data={items.data}
@@ -31,8 +50,9 @@ export default function MDatatable({ items, resource ,columns}: { items: Paginat
                     from: items.from,
                     to: items.to,
                     per_page: items.per_page,
-                    total: items.total
-                }} />
+                    total: items.total,
+                }}
+            />
         </>
     );
 }
